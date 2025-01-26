56dfea586f41106a4b01a409f638b20594bd15bf
khushi patel <khushihpatel2008@gmail.com>
2025-01-26 17:22:52 +0000

    styling on todo list and validation

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,34 +1,84 @@
-import { View, Text, TextInput, Button, FlatList } from 'react-native'
+import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 
 const ToDo = () => {
     const [todos, setTodos] = useState(["Eat", "Sleep", "Go outside"])
     const [todo, setTodo] = useState("")
-    const ToDoItem = ({item}:{item:string}) => {
+    const ToDoItem = ({ item }: { item: string }) => {
         return (
-            <View >
+            <View style={styles.itemCard}>
                 <Text> {item}</Text>
+
+                <Button title='Edit'
+                    color='magenta'
+                    onPress={() => {
+                        alert("Edit")
+                    }}
+                />
                 <Button title='Delete'
+                    color='#E35335'
                     onPress={() => {
                         setTodos(todos.filter(todo => todo !== item))
-                    }}/>
+                    }} />
             </View>
         )
     }
 
-  return (
-    <View>
-      <Text>To Do:</Text>
-      <TextInput placeholder='Enter to do:' value={todo} onChangeText={setTodo}/>
-      <Button title='Add' 
-        onPress={() => {setTodos([...todos,todo])
-            setTodo("")
-        }}/>
-    <FlatList data={todos} 
-    keyExtractor={(item,index) => index.toString()}
-    renderItem={ToDoItem}/>
-    </View>
-  )
+    return (
+        <View >
+            <Text style={styles.todoTitle}>To Do:</Text>
+            <TextInput style={styles.addItem} placeholder='Enter to do:' value={todo} onChangeText={setTodo} />
+            <Button title='Add'
+                color='#50C878'
+                onPress={() => {
+                    if (todo !== null && todo !== "") {
+                        if (!todos.includes(todo)) {
+                            setTodos([...todos, todo])
+                            setTodo("")
+                        }
+                        else { alert("Already in To Do list") }
+                    }
+                    else { alert("Please enter valid input") }
+                }} />
+
+            <FlatList data={todos}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={ToDoItem} />
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    todoTitle: {
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#57A1A0',
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 30
+    },
+
+    itemCard: {
+        padding: 10,
+        margin: 10,
+        backgroundColor: 'lightgrey',
+        borderRadius: 15,
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+
+    addItem: {
+        padding: 10,
+        margin: 10,
+        backgroundColor: 'lightgrey',
+        borderRadius: 15,
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+})
 
 export default ToDo
